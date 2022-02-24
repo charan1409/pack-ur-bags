132e1fb14468e96764cabb53e87710059f231e6d
charan14092003 <charankallempudi24@gmail.com>
2022-02-24 15:39:34 +0530

    commit

--- PUB/script.js
+++ PUB/script.js
@@ -38,7 +38,7 @@ videoBtn.forEach(btn =>{
         document.querySelector('.controls .active').classList.remove('active');
         btn.classList.add('active');
         let src = btn.getAttribute('data-src');
-        document.querySelector('#video-slider').src = src;
+        document.querySelector('video-slider').src = src;
     });
 });
 
