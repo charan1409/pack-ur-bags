25a689241ef9b51b5f3a34a8dac4b3110f491591
charan14092003 <charankallempudi24@gmail.com>
2022-03-13 15:06:35 +0530

    commit

--- PUB/js/login.js
+++ PUB/js/login.js
@@ -1,40 +1,37 @@
 var form = document.getElementById('log');
 
-    form.addEventListener('mousemove', (e) =>{
+form.addEventListener('mousemove', (e) => {
 
-      var x = (window.innerWidth / 2 -e.pageX) / 12;
-      var y = (window.innerHeight / 2 -e.pageY) / 12;
+  var x = (window.innerWidth / 2 - e.pageX) / 12;
+  var y = (window.innerHeight / 2 - e.pageY) / 12;
 
-      form.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)'
+  form.style.transform = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)'
 
-    });
+});
 
-    form.addEventListener('mouseleave', function(){
+form.addEventListener('mouseleave', function () {
 
-      form.style.transform = 'rotateX(0deg) rotateY(0deg)';
-
-
-    });
+  form.style.transform = 'rotateX(0deg) rotateY(0deg)';
 
 
+});
 
+function login() {
+  let x = document.getElementById("login");
+  let y = document.getElementById("register");
+  let z = document.getElementById("stat-btn")
 
-    function login(){
-        let x = document.getElementById("login");
-        let y = document.getElementById("register");
-        let z = document.getElementById("stat-btn")
-    
-        x.style.left = "0";
-        y.style.left = "600px";
-        z.style.left = "40px";
-    }
-    
-    function register(){
-        let x = document.getElementById("login");
-        let y = document.getElementById("register");
-        let z = document.getElementById("stat-btn")
-    
-        x.style.left = "-600px";
-        y.style.left = "0";
-        z.style.left = "270px";
-    }
+  x.style.left = "0";
+  y.style.left = "600px";
+  z.style.left = "40px";
+}
+
+function register() {
+  let x = document.getElementById("login");
+  let y = document.getElementById("register");
+  let z = document.getElementById("stat-btn")
+
+  x.style.left = "-600px";
+  y.style.left = "0";
+  z.style.left = "270px";
+}
