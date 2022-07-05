9012eeb0bd97cfb436941045eb1e1acc5eda73c5
charan14092003 <charankallempudi24@gmail.com>
2022-07-05 17:08:15 +0530

    index page navbar

--- PUB/public/js/script.js
+++ PUB/public/js/script.js
@@ -1,3 +1,26 @@
+const navToggler = document.querySelector(".nav-toggler");
+ navToggler.addEventListener("click", navToggle);
+
+ function navToggle() {
+    navToggler.classList.toggle("active");
+    const nav = document.querySelector(".links");
+    nav.classList.toggle("open");
+    if(nav.classList.contains("open")){
+    	nav.style.left = '-88px';
+    } else{
+      nav.style.left = '-888px';
+    }
+ }
+ 
+ window.addEventListener('scroll',()=>{
+  const nav = document.querySelector('.links');
+  if(nav.classList.contains("open")){
+    navToggler.classList.toggle("active");
+    nav.style.left = '-888px';
+    nav.classList.remove('open',window.scrollY > 0)
+  }
+})
+
 function show(id) {
   let x = document.getElementById(id);
   if (x.style.display === 'block') {
