8771f4b3c190f5c73c7d5577b7e352e21693e533
charan14092003 <charankallempudi24@gmail.com>
2022-07-08 14:06:51 +0530

    index submenu

--- PUB/public/js/script.js
+++ PUB/public/js/script.js
@@ -1,35 +1,44 @@
 const navToggler = document.querySelector(".nav-toggler");
 const navProfile = document.querySelector(".navprofile");
-navToggler.addEventListener("click", () =>{
+const submenu = document.querySelector(".sub-menu");
+navToggler.addEventListener("click", () => {
   navToggler.classList.toggle("active");
   const nav = document.querySelector(".links");
   nav.classList.toggle("open");
-  if(nav.classList.contains("open")){
+  if (nav.classList.contains("open")) {
     nav.style.left = '-88px';
-  } else{
+  } else {
     nav.style.left = '-8888px';
   }
 });
 
-navProfile.addEventListener("click",() =>{
+navProfile.addEventListener("click", () => {
   const nav = document.querySelector(".sub-menu");
   const down = document.querySelector("#down");
   nav.classList.toggle("show");
   down.classList.toggle("bx-chevron-up")
 })
- 
- window.addEventListener('scroll',()=>{
-  const nav = document.querySelector('.links');
-  const navp = document.querySelector(".sub-menu");
-  if(nav.classList.contains("open")){
-    navToggler.classList.toggle("active");
-    nav.style.left = '-888px';
-    nav.classList.remove('open',window.scrollY > 0)
-  }
-  if(navp.classList.contains("show")){
+
+submenu.addEventListener('mouseleave',() =>{
+  if (submenu.classList.contains("show")) {
     const down = document.querySelector("#down");
     down.classList.remove("bx-chevron-up")
-    navp.classList.remove('show',window.scrollY > 0)
+    submenu.classList.remove('show', window.scrollY > 0)
+  }
+})
+
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('.links');
+  const navp = document.querySelector(".sub-menu");
+  if (nav.classList.contains("open")) {
+    navToggler.classList.toggle("active");
+    nav.style.left = '-888px';
+    nav.classList.remove('open', window.scrollY > 0)
+  }
+  if (navp.classList.contains("show")) {
+    const down = document.querySelector("#down");
+    down.classList.remove("bx-chevron-up")
+    navp.classList.remove('show', window.scrollY > 0)
   }
 })
 
