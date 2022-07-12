bf3d3e6954cecfcdb272770c5c9fd365b1414590
charan14092003 <charankallempudi24@gmail.com>
2022-07-12 17:36:07 +0530

    commit

--- PUB/public/js/script.js
+++ PUB/public/js/script.js
@@ -10,21 +10,6 @@ navToggler.addEventListener("click", () => {
   }
 });
 
-// navProfile.addEventListener("click", () => {
-//   const nav = document.querySelector(".sub-menu");
-//   const down = document.querySelector("#down");
-//   nav.classList.toggle("show");
-//   down.classList.toggle("bx-chevron-up")
-// })
-
-// submenu.addEventListener('mouseleave',() =>{
-//   if (submenu.classList.contains("show")) {
-//     const down = document.querySelector("#down");
-//     down.classList.remove("bx-chevron-up")
-//     submenu.classList.remove('show', window.scrollY > 0)
-//   }
-// })
-
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('.links');
   if (nav.classList.contains("open")) {
