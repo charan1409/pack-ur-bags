b4967bf8009e035ee41decdbaae6a923ad864c6c
charan14092003 <charankallempudi24@gmail.com>
2022-07-13 16:18:08 +0530

    commit

--- PUB/public/js/navbar.js
+++ PUB/public/js/navbar.js
@@ -1,35 +1,30 @@
 const navProfile = document.querySelector(".navprofile");
 const submenu = document.querySelector(".sub-menu");
+const down = document.querySelector("#down");
 
 navProfile.addEventListener("click", () => {
-    const nav = document.querySelector(".sub-menu");
-    const down = document.querySelector("#down");
-    nav.classList.toggle("show");
-    down.classList.toggle("bx-chevron-up")
+    console.log('called 1 time');
+    submenu.classList.toggle("show");
+    down.classList.toggle("bx-chevron-up");
 })
 
 submenu.addEventListener('mouseleave', () => {
     if (submenu.classList.contains("show")) {
-        const down = document.querySelector("#down");
         down.classList.remove("bx-chevron-up")
         submenu.classList.remove('show', window.scrollY > 0)
     }
 })
 
 window.addEventListener('scroll', () => {
-    const navp = document.querySelector(".sub-menu");
-    if (navp.classList.contains("show")) {
-        const down = document.querySelector("#down");
+    if (submenu.classList.contains("show")) {
         down.classList.remove("bx-chevron-up")
-        navp.classList.remove('show', window.scrollY > 0)
+        submenu.classList.remove('show', window.scrollY > 0)
     }
 })
 
 window.addEventListener('mouseup', () => {
-    const navp = document.querySelector(".sub-menu");
-    if (navp.classList.contains("show")) {
-        const down = document.querySelector("#down");
+    if (submenu.classList.contains("show")) {
         down.classList.remove("bx-chevron-up")
-        navp.classList.remove('show', window.scrollY > 0)
+        submenu.classList.remove('show', window.scrollY > 0)
     }
 })
