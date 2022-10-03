5e97319d9cc144bdfddd920936d259a26e653d24
rahulvarma5297 <rahulvarma5297@gmail.com>
2022-10-03 14:46:03 +0530

    IndexPageComponent

---
+++ client/src/Components/Main/Index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./main.css";
+import Upward from "./Upward";
+import Header from "./Header";
+import Vedio from "./Vedio";
+import Gallery from "./Gallery";
+import Places from "./Places";
+import About from "./About";
+import Services from "./Services";
+import Review from "./Review";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+
+const Index = () => {
+  return (
+    <div>
+      <Upward />
+      <Header />
+      <Vedio />
+      <Gallery />
+      <Places />
+      <About />
+      <Services />
+      <Review />
+      <Feedback />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
