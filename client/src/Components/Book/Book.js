8f26efb987cf3119f9724e0f65d4b9758323e4bd
charan14092003 <charankallempudi24@gmail.com>
2022-10-10 15:38:38 +0530

    navbar css position

--- client/src/Components/Book/Book.js
+++ client/src/Components/Book/Book.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../Main/Header";
 
 const Book = () => {
   return (
     <div>
+      <Header />
       <h1>Booking Page Here.</h1>
       <Link to="/payment"><h1>Link to Payment Page</h1></Link>
     </div>
