6e9051997d825bffce95d19aefa5063001d5af8d
charan14092003 <charankallempudi24@gmail.com>
2022-10-10 15:41:49 +0530

    commit

--- client/src/Components/Book/Book.js
+++ client/src/Components/Book/Book.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Header from "../Main/Header";
 
 const Book = () => {
   return (
     <div>
-      <Header />
       <h1>Booking Page Here.</h1>
       <Link to="/payment"><h1>Link to Payment Page</h1></Link>
     </div>
