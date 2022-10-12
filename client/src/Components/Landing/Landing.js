97fb63d01421def6dda13de66f4ff5de906da9d6
charan14092003 <charankallempudi24@gmail.com>
2022-10-12 22:35:29 +0530

    commit

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -5,7 +5,6 @@ import Vedio from "../Main/Vedio";
 import Gallery from "../Main/Gallery";
 import Services from "../Main/Services";
 import LoginPage from "./LoginPage";
-import { Link } from "react-router-dom";
 
 
 
@@ -22,7 +21,6 @@ const Landing = () => {
       <Vedio />
       <Gallery />
       <Services />
-      <Link to="/index"><h1>Link to Index Page</h1></Link>
       {login && <LoginPage openLoginForm={toggleLoginForm}/>}
     </div>
   )
