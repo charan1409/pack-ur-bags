513541addaa8e667bf9749b39993eba69ffe4bd2
rahulvarma5297 <rahulvarma5297@gmail.com>
2022-10-27 19:59:33 +0530

    error page

--- client/src/Components/ErrorPage/Error.js
+++ client/src/Components/ErrorPage/Error.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import './error.css'
+import React from "react";
+import "./error.css";
+import { Link } from "react-router-dom";
+import vid1 from "./img/boy.mp4";
+import vid2 from "./img/dragon.mp4";
 
 const Error = () => {
   return (
-    <>
-    <h1>404 ERROR</h1>
-    </>
-  )
-}
+    <div className="error">
+      <h1>404 ERROR</h1>
+      <div className="video-container">
+        <video src={vid1} loop autoPlay muted className="vid1"></video>
+        <video src={vid2} loop autoPlay muted className="vid2"></video>
+      </div>
+      <h1>
+        Looks Like Landed Some Where Else{" "}
+        <Link to="/index" className="btn new-btn" style={{ margin: "auto" }}>
+          GO BACK
+        </Link>
+      </h1>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
