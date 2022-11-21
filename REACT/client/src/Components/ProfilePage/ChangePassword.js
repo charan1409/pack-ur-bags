3e2cffd14ad36e7b3b1d1c059b46ae0f77a5f4d3
charan14092003 <charankallempudi24@gmail.com>
2022-11-21 18:50:22 +0530

    commit

--- REACT/client/src/Components/ProfilePage/ChangePassword.js
+++ REACT/client/src/Components/ProfilePage/ChangePassword.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import InputBox from "../Landing/InputBox";
 
 function ChangePassword() {
   const user = JSON.parse(localStorage.getItem("user"));
