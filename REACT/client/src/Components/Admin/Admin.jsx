091278f28c5016b780f8e10a6f7c59386decd2c4
rahulvarma5297 <rahulvarma5297@gmail.com>
2022-12-02 23:59:39 +0530

    search_done

--- REACT/client/src/Components/Admin/Admin.jsx
+++ REACT/client/src/Components/Admin/Admin.jsx
@@ -7,6 +7,8 @@ import Header from "../Navbar/Header";
 
 function Admin() {
   const [userData, setuserData] = useState([]);
+  const [state, setState] = useState([]);
+  const [datanotfound, setdatanotfound] = useState(true);
   useEffect(() => {
     axios.get(`http://localhost:3001/users`).then((res) => {
       setuserData(res.data);
@@ -35,7 +37,15 @@ function Admin() {
             type="text"
             placeholder="Search for User"
             onChange={(e) => {
-              
+              const search = e.target.value.trimStart().trimEnd();
+              setdatanotfound(true);
+              const data = userData.filter((item) => {
+                return item.username.toLowerCase().includes(search.toLowerCase());
+              });
+              if (data.length === 0) {
+                setdatanotfound(false);
+              }
+              setState(data);
             }}
           />
           <i className="fas fa-search"></i>
@@ -49,7 +59,7 @@ function Admin() {
               <th>Tours</th>
               <th>Delete</th>
             </tr>
-            {userData.map((x) => (
+            {/* {userData.map((x) => (
               <tr>
                 <td>{x.username}</td>
                 <td>{x.email}</td>
@@ -70,7 +80,63 @@ function Admin() {
                   <Btn value="Delete" onClick={() => Deluser(x.id)} />
                 </td>
               </tr>
-            ))}
+            ))} */}
+
+            {datanotfound ? (
+              state.length === 0 ? (
+                userData.map((x) => {
+                  return (
+                    <tr>
+                    <td>{x.username}</td>
+                    <td>{x.email}</td>
+                    <td>{x.password}</td>
+                    <td>
+                      {x.tours.map((item) => (
+                        <div>
+                          <h3>From - {item.from}</h3>
+                          <h3>To - {item.to}</h3>
+                          <h3>Number of Adults- {item.adult}</h3>
+                          <h3>Number of Child- {item.child}</h3>
+                          <h3>Date of Departure- {item.depart}</h3>
+                          <h3>Date of Arrival- {item.arrival}</h3>
+                        </div>
+                      ))}
+                    </td>
+                    <td>
+                      <Btn value="Delete" onClick={() => Deluser(x.id)} />
+                    </td>
+                  </tr>
+                  );
+                })
+              ) : (
+                state.map((x) => {
+                  return (
+                    <tr>
+                    <td>{x.username}</td>
+                    <td>{x.email}</td>
+                    <td>{x.password}</td>
+                    <td>
+                      {x.tours.map((item) => (
+                        <div>
+                          <h3>From - {item.from}</h3>
+                          <h3>To - {item.to}</h3>
+                          <h3>Number of Adults- {item.adult}</h3>
+                          <h3>Number of Child- {item.child}</h3>
+                          <h3>Date of Departure- {item.depart}</h3>
+                          <h3>Date of Arrival- {item.arrival}</h3>
+                        </div>
+                      ))}
+                    </td>
+                    <td>
+                      <Btn value="Delete" onClick={() => Deluser(x.id)} />
+                    </td>
+                  </tr>
+                  );
+                })
+              )
+            ) : (
+              <h1 className="notfound">No Data Found</h1>
+            )}
           </table>
         </div>
       </div>
