eb8e458b89006542c272c3390159f022b2356eb6
rahulvarma5297 <rahulvarma5297@gmail.com>
2023-02-10 22:14:53 +0530

    centered

--- client/src/Components/Loading/Loading.js
+++ client/src/Components/Loading/Loading.js
@@ -3,17 +3,15 @@ import "./Loading.css";
 
 const Loading = () => {
   return (
-    <div>
-      <div class="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
