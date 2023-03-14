ca3d97f82a841384cecd6aeec9587bbbb7086798
rahulvarma5297 <rahulvarma5297@gmail.com>
2023-03-14 17:53:35 +0530

    profile page

--- client/src/Components/ProfilePage/Profile.js
+++ client/src/Components/ProfilePage/Profile.js
@@ -25,8 +25,8 @@ function Profile(props) {
   const hiddenFileInput = useRef(null);
   const userval = props.user.user.user;
   const [user, setUser] = useState({});
-  const [fd, setFd] = useState({})
-  const [edit, setEdit] = useState(false)
+  const [fd, setFd] = useState({});
+  const [edit, setEdit] = useState(false);
   // eslint-disable-next-line
   const userL = JSON.parse(localStorage.getItem("user"));
   const fetchData = async () => {
@@ -36,13 +36,13 @@ function Profile(props) {
         console.log(resp.data);
         localStorage.setItem("user", JSON.stringify(resp.data.user));
         dispatch(actionCreators.user(resp.data));
-        setFd(resp.data.fd)
+        setFd(resp.data.fd);
         return setUser(resp.data.user);
       });
   };
   useEffect(() => {
     fetchData();
-    setEdit(false)
+    setEdit(false);
     // eslint-disable-next-line
   }, []);
 
@@ -50,18 +50,19 @@ function Profile(props) {
     if (e.target.files[0]) {
       const fd = new FormData();
       fd.append("image", e.target.files[0], e.target.files[0].name);
-      fd.append("email", userval.email)
+      fd.append("email", userval.email);
       axios.post("http://localhost:9000/profile/upload", fd, {
-        headers: { "Content-Type": "multipart/form-data" }
-      })
+        headers: { "Content-Type": "multipart/form-data" },
+      });
       await axios
         .post("http://localhost:9000/profile/upload", fd)
         .then((resp) => {
           if (resp.status === 200) {
-            axios.get(`http://localhost:9000/users/loguser/${userL.username}`)
+            axios
+              .get(`http://localhost:9000/users/loguser/${userL.username}`)
               .then(async (response) => {
                 dispatch(actionCreators.user(response.data));
-              })
+              });
             alert(resp.data.succ);
           }
         });
@@ -75,10 +76,11 @@ function Profile(props) {
       .post("http://localhost:9000/profile/remove", fd)
       .then((resp) => {
         if (resp.status === 200) {
-          axios.get(`http://localhost:9000/users/loguser/${userL.username}`)
+          axios
+            .get(`http://localhost:9000/users/loguser/${userL.username}`)
             .then(async (response) => {
               dispatch(actionCreators.user(response.data));
-            })
+            });
           alert(resp.data.succ);
         }
       });
@@ -94,10 +96,7 @@ function Profile(props) {
             <div className="profile">
               <div className="_left">
                 <div className="_img">
-                  <img
-                    src={user.image}
-                    alt="profile_img"
-                  />
+                  <img src={user.image} alt="profile_img" />
                   <div className="img_content">
                     <i
                       className="bi bi-upload fa-3x"
@@ -109,7 +108,6 @@ function Profile(props) {
                       ref={hiddenFileInput}
                       name="image"
                       onChange={profileHandler}
-
                     ></input>
                     {user.imagegiven && (
                       <i
@@ -121,11 +119,13 @@ function Profile(props) {
                 </div>
               </div>
               <div className="_right">
-                {edit ? <div className="edit-form">
-                  <h2>edit your profile</h2>
-                  <Edityourprofile username="User" />
-                  <span onClick={() => setEdit(false)}>x</span>
-                </div> :
+                {edit ? (
+                  <div className="edit-form">
+                    <h2>edit your profile</h2>
+                    <Edityourprofile username="User" />
+                    <span onClick={() => setEdit(false)}>x</span>
+                  </div>
+                ) : (
                   <table>
                     <Tabledata heading={"Username:"} data={userval.username} />
                     <Tabledata heading={"Name:"} data={userval.name} />
@@ -135,12 +135,17 @@ function Profile(props) {
                       heading={"Phone Number:"}
                       data={userval.phonenumber}
                     />
-                  </table>}
+                  </table>
+                )}
                 <div className="btns">
-                  {!edit && <button className="btn_profile" onClick={() => setEdit(true
-                  )}>
-                    edit profile
-                  </button>}
+                  {!edit && (
+                    <button
+                      className="btn_profile"
+                      onClick={() => setEdit(true)}
+                    >
+                      edit profile
+                    </button>
+                  )}
                   <button className="btn_profile" onClick={change_pass}>
                     change password
                   </button>
@@ -149,23 +154,26 @@ function Profile(props) {
               </div>
             </div>
           </div>
-          <hr style={{ height: "1px", backgroundColor: "black" }} />
-          <div className="latest-upcoming-booking">
-            <h2>
-              Your latest upcoming booking will appear here with view all
-              booking buttons
-            </h2>
-          </div>
-          <hr style={{ height: "1px", backgroundColor: "black" }} />
-          <div className="user-feedback">
-            <h2>{fd ? fd.feedback : "Your submitted feedback will appear here."}</h2>
-          </div>
-          <hr style={{ height: "1px", backgroundColor: "black" }} />
-          <div className="user-reviews">
-            <h2>Your reviews to your tours will appear here with edit option.</h2>
-          </div>
         </div>
       </div>
+      {/* <hr style={{ height: "100px", backgroundColor: "black" }} /> */}
+      <div className="latest-upcoming-booking">
+        <h2>
+          Your latest upcoming booking will appear here with view all booking
+          buttons
+        </h2>
+      </div>
+      <hr style={{ height: "1px", backgroundColor: "black" }} />
+      <div className="user-feedback">
+        <h2>
+          {fd ? fd.feedback : "Your submitted feedback will appear here."}
+        </h2>
+      </div>
+      <hr style={{ height: "1px", backgroundColor: "black" }} />
+      <div className="user-reviews">
+        <h2>Your reviews to your tours will appear here with edit option.</h2>
+      </div>
+
       <div className="pass-modal-bg">
         <div className="edit-form">
           <h2>Change your password</h2>
