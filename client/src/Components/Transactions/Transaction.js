3dc36d9c8e907b9b94edda9cdaeb79533722e0a2
rahulvarma5297 <rahulvarma5297@gmail.com>
2023-03-22 13:49:23 +0530

    loading added

--- client/src/Components/Transactions/Transaction.js
+++ client/src/Components/Transactions/Transaction.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Header from "../Navbar/Header";
 import "./Trans.css";
 import axios from "axios";
+import Loading from "../Loading/Loading";
 
 const Transaction = (props) => {
-
   const [user, setUser] = useState({});
   const [trans, setTrans] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const removeItems = (key) => {
     const newset = trans.filter((ob, a) => a !== key);
@@ -20,9 +20,12 @@ const Transaction = (props) => {
       .then((resp) => {
         return setUser(resp.data.user);
       });
-    axios.get(`http://localhost:9000/payment/getTransactions/${userL.username}`).then((resp) => {
-      return setTrans(resp.data);
-    });
+    axios
+      .get(`http://localhost:9000/payment/getTransactions/${userL.username}`)
+      .then((resp) => {
+        setLoading(false);
+        return setTrans(resp.data);
+      });
   }, []);
 
   const navItems = [
@@ -47,39 +50,42 @@ const Transaction = (props) => {
   return (
     <div>
       <Header user={user} navItems={navItems} />
-      <Link to="/payment">
-        <input type="submit" className="btn" value="Payment" />
-      </Link>
-      {trans.length === 0 ? (
-        <h1>Transactions are Empty</h1>
+      {loading ? (
+        <Loading />
       ) : (
         <div>
-          <div className="item-class">
-            {trans
-              ? trans.map((item, key) => {
-                return (
-                  <div className="item-box" key={key}>
-                    <h2>Card Number : {item.number}</h2>
-                    <h2>Name : {item.name}</h2>
-                    <h2>Month : {item.month}</h2>
-                    <h2>Year : {item.year}</h2>
-                    <h2>CVV: {item.cvv}</h2>
-                    <div className="addtocart">
-                      <button
-                        onClick={() => {
-                          removeItems(key);
-                          alert("You will contacted for Refund");
-                        }}
-                        className="book-btn btn-primary"
-                      >
-                        Cancel and Refund{" "}
-                      </button>
-                    </div>
-                  </div>
-                );
-              })
-              : ""}
-          </div>
+          {trans.length === 0 ? (
+            <h1>Transactions are Empty</h1>
+          ) : (
+            <div>
+              <div className="item-class">
+                {trans
+                  ? trans.map((item, key) => {
+                      return (
+                        <div className="item-box" key={key}>
+                          <h2>Card Number : {item.number}</h2>
+                          <h2>Name : {item.name}</h2>
+                          <h2>Month : {item.month}</h2>
+                          <h2>Year : {item.year}</h2>
+                          <h2>CVV: {item.cvv}</h2>
+                          <div className="addtocart">
+                            <button
+                              onClick={() => {
+                                removeItems(key);
+                                alert("You will contacted for Refund");
+                              }}
+                              className="book-btn btn-primary"
+                            >
+                              Cancel and Refund{" "}
+                            </button>
+                          </div>
+                        </div>
+                      );
+                    })
+                  : ""}
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
