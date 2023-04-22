8769a10b756689cd9ea07c831770a2a073ddb6c5
rahulvarma5297 <rahulvarma5297@gmail.com>
2023-04-22 16:12:35 +0530

    production

--- client/src/AxiosConfig.js
+++ client/src/AxiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getBaseUrl = () => {
   let url;
-  switch ("asdf") {
+  switch ("production") {
     case "production":
       url = "https://backend-packurbags.onrender.com/";
       break;
